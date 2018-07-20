0fa454ffcddc73160aca9ccec0c993bae4b8298e
Ivan Martinez Morales <35318080+ivanmartinezmorales@users.noreply.github.com>
2018-07-20 10:34:08 -0700

    Update linkedlist.py

--- linkedlist.py
+++ linkedlist.py
@@ -1,8 +1,29 @@
 '''
 Learning about linked lists!
+
+First, I created a class for nodes. The node holds data for every item in the linked list.
+Then in the singlyLinked class, __init__ declares the head. 
 '''
 
 # Creating a node:
 
-class Node(object):
+class Node:
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+# Creating the single linked list        
+class singlyLinked:
+    def __init__(self):
+        self.head = None
     
+    # Method to traverse LL
+    def printList(self):
+        printValue = self.head
+        while printValue is not None:
+            print(printValue.data)
+            printValue = printValue.next
+            
+    def insertFirst(self, new_node):
+        newNode = Node(new_node)
+        
+  
