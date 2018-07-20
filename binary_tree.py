e2d0f6a9dec102d1be7628769641f3a66ddc7e74
Ivan Martinez Morales <35318080+ivanmartinezmorales@users.noreply.github.com>
2018-07-20 13:01:30 -0700

    Update binary_tree.py

--- binary_tree.py
+++ binary_tree.py
@@ -16,8 +16,37 @@ they will have the following variables:
 '''
 
 class Node:
+    
     def __init__(self, data):
         self.left = None
         self.right = None
         self.data = data
-        
+
+'''
+Function to insert 
+'''
+    def insert(self, data):
+        if self.data:
+            if data < self.data: 
+            # if our inserted node is less than the parent node, then insert left
+                if self.left is None:
+                    self.left = Node(data)
+                else:
+                    self.left.insert(data)
+            elif data > self.data:
+            # if our inserted data is greater than our parent node, insert right.
+                if self.right is None:
+                    self.right = Node(data)
+                else:
+                    self.right.insert(data)
+        else:
+            self.data = data
+            
+    def printTree(self):
+        if self.left:
+            self.left.printTree()
+        print(self.data)
+        if self.right:
+            self.right.printTree()
+            
+            
