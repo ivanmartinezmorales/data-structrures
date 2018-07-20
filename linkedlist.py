0636a3ccc3550005ff69603f51803ad3e7824cb3
Ivan Martinez Morales <35318080+ivanmartinezmorales@users.noreply.github.com>
2018-07-20 10:57:11 -0700

    Update linkedlist.py

--- linkedlist.py
+++ linkedlist.py
@@ -16,14 +16,40 @@ class singlyLinked:
     def __init__(self):
         self.head = None
     
-    # Method to traverse LL
+    # printing linked list
     def printList(self):
         printValue = self.head
         while printValue is not None:
             print(printValue.data)
             printValue = printValue.next
             
+    # Inserting at the beginning of the existing list
     def insertFirst(self, new_node):
         newNode = Node(new_node)
+        NewNode.next = self.head
+        self.head = newNode
         
-  
+    # Inserting at the end of the linked list    
+    def insertLast(self, new_node):
+        newNode = Node(new_node)
+        if self.head is None:
+            self.head = newNode
+            return
+        last = self.head
+        while(last.next):
+            last = last.next
+        last.next = newNode
+        
+    # method to insert a node in the middle of the list    
+    def insertInside(self, middle_node, new_node):
+        if middle_node is None:
+            print("The mentioned node is absent")
+            return
+        
+        newNode = Node(new_node)
+        newNode.next = middle_node.next
+        middle_node.next = newNode
+    
+    def removeNode(self, node_to_remove):
+        
+   
