3d21cb3b23fbf27d37062da26e33cfa3aafba4a1
Ivan Martinez Morales <35318080+ivanmartinezmorales@users.noreply.github.com>
2018-07-26 22:23:41 -0700

    Update binary-search.py

--- binary-search.py
+++ binary-search.py
@@ -3,10 +3,12 @@ Created a binary search tree in like 15 minutes... sorry for mistakes :)
 '''
 
 class Node:
-	def __init__(self, data):
-		self.data = data
-		self.left = None
-		self.right = None
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+
 
 '''
 okay, got side tracked... will revisit thiss
