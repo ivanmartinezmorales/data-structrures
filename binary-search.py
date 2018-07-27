28c7ae10e23067b85fae350102f8d784c684d61e
Ivan Martinez Morales <35318080+ivanmartinezmorales@users.noreply.github.com>
2018-07-26 22:28:42 -0700

    Created insert method

--- binary-search.py
+++ binary-search.py
@@ -7,9 +7,22 @@ class Node:
         self.data = data
         self.left = None
         self.right = None
-
-
-
+        
+    def insert(self, data):
+        if self.data:
+            if data < self.data:
+                if self.left is None:
+                    self.left = Node(data)
+                else:
+                    self.left.insert(data) # using recursion??
+            elif data > self.data:
+                if self.right is None:
+                    self.right = Node(data)
+                else:
+                    self.right.insert(data) # using recursion? hmm
+        else:
+            self.data = data
+           
 '''
-okay, got side tracked... will revisit thiss
+
 '''
