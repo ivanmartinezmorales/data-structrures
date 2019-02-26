adffff44ac81f7a55812721118ce359e879d602e
Ivan Martinez Morales <35318080+ivanmartinezmorales@users.noreply.github.com>
2019-02-25 20:45:24 -0700

    Update singlyLinked.py

--- singlyLinked.py
+++ singlyLinked.py
@@ -1,104 +1,71 @@
-'''
-Learning about linked lists!
+"""
+My implementation of a Singly Linked List.
+"""
 
-First, I created a class for nodes. The node holds data for every item in the linked list.
-Then in the singlyLinked class, __init__ declares the head. 
-'''
-
-# Creating a node:
 class Node:
-    def __init__(self, data=None):
+    def __init__(self, data, next=None):
         self.data = data
-        self.next = None
-        
-# Creating the single linked list        
-class singlyLinked:
+        self.next = next
+    
+    def __repr__(self):
+        return repr(self.data)
+    
+
+class SinglyLinkedList:
     def __init__(self):
         self.head = None
-    
-    # printing linked list
-    def printList(self):
-        printValue = self.head
-        while printValue is not None:
-            print(printValue.data)
-            printValue = printValue.next
+        self.tail = self.head
+        
+    def prepend(self, head):
+        if head == None:
+            raise Exception
+        if self.head == None:
+            self.head = Node(head)
             
-    # Inserting at the beginning of the existing list
-    def insertFirst(self, new_node):
-        newNode = Node(new_node)
-        NewNode.next = self.head
-        self.head = newNode
-        
-    # Inserting at the end of the linked list    
-    def insertLast(self, new_node):
-        newNode = Node(new_node)
-        if self.head is None:
-            self.head = newNode
-            return
-        last = self.head
-        while(last.next):
-            last = last.next
-        last.next = newNode
-        
-    # method to insert a node in the middle of the list    
-    def insertInside(self, middle_node, new_node):
-        if middle_node is None:
-            print("The mentioned node is absent")
-            return
-        
-        newNode = Node(new_node)
-        newNode.next = middle_node.next
-        middle_node.next = newNode
+    def add_new_head(self, new_head):
+        self.head = Node(new_head, self.head)
     
-    # Method to remove a given node in the linked list.
-    def removeNode(self, node_to_remove):
-        headValue = self.head
-        if (headValue is not None):
-            if (headValue.data == node_to_remove):
-                self.head = headValue.next
-                headValue = None
-                return
-            
-        while (headValue is not None):
-            if headValue.data == node_to_remove:
+    def print_elements(self):
+        if self.head == None:
+            return None
+        if self.head == self.tail:
+            return self.head
+        curr = self.head
+        while curr:
+            print(repr(curr), "-->")
+            curr = curr.next
+            if curr == None:
+                print("end of the list!")
                 break
-            prev = headValue
-            headValue = headValue.next
-            
-        if (headValue == None):
-            return
+    
+    def append(self, data):
+        if not self.head:
+            self.head = Node(data)
+        curr = self.head
+        while curr.next:
+            curr = curr.next
+        curr.next = Node(data)
+    
+    def find(self, element):
+        curr = self.head
+        while curr and curr.data != element:
+            curr = curr.next
         
-        prev.next = headValue.next
-        headValue = None
-'''
-Here, I am calling our linked list, performing the following operations:
--Creating the linked list as our_list
--Creating three nodes, and linking them together. (l1, l2, l3)
-'''
-our_list = singlyLinked()
-our_list.head = ("first item")
-
-l2 = Node("second item")
-l3 = Node("third item")
-
-# Linking the three Nodes together
-our_list.head.next = l2
-l2.next = l3
-
-# Inserting a Node at the beginning of the list:
-our_list.insertFirst("zero-th item")
-
-# Inserting a node at the end of the list:
-our_list.insertLast("fourth item")
-
-# Inserting in the middle of the list:
-our_list.head.next = l3
-our_list.insertInside(our_list.head.next, "weird item")
-
-# Removing the weird node we just created:
-our_list.removeNode("weird item")
-
-# Printing our final list:
-our_list.print_list()
-
-
+        return curr
+    
+    def remove(self, item):
+        if not item:
+            raise Exception
+        
+        curr = self.head
+        prev = None
+        while curr and curr.data != item:
+            prev = curr
+            curr = curr.next
+        
+        if prev is None:
+            self.head = curr.next
+        elif curr:
+            prev.next = curr.next
+            curr.next = None
+        
